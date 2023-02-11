36ab521ec2df387408d8f0fc9744fe959dcd7570
kenan <kenanmesic1191@gmail.com>
2023-02-11 11:35:14 -0800

    third

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+
+const userRoutes = require('./userRoutes.js');
+const postRoutes = require('./post.js');
+const blogRoutes = require('./blogRoutes.js');
 
 router.use('/users', userRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 
 module.exports = router;
