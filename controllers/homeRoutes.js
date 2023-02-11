d2ebc8bae55fcfd6c662dca56333b5ef59432b6f
kenan <kenanmesic1191@gmail.com>
2023-02-11 14:17:40 -0800

    controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 router.get('/', async (req, res) => {
-    try {
-      // Get all projects and JOIN with user data
+  try {
+    // Get all projects and JOIN with user data
     //   const projectData = await Project.findAll({
     //     include: [
     //       {
@@ -10,29 +10,29 @@ router.get('/', async (req, res) => {
     //       },
     //     ],
     //   });
-  
-      // Serialize data so the template can read it
+
+    // Serialize data so the template can read it
     //   const projects = projectData.map((project) => project.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        // projects, 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      // projects, 
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect('/profile');
-      return;
-    }
-  
-    res.render('login');
-  });
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
 
 
 module.exports = router;
